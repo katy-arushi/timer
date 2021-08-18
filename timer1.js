195478a0331e55cd7a03686e0f15d2e4dd51e97b
Arushi Katyal <katy.arushi@gmail.com>
2021-08-17 23:40:08 -0400

    add edge cases

--- timer1.js
+++ timer1.js
@@ -1,7 +1,11 @@
 const alarms = process.argv.slice(2);
 
 for (let i = 0; i < alarms.length; i++) {
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, (alarms[i] * 1000))
+  if ((isNaN(alarms[i])) || alarms[i] <= 0) {
+    i++;
+  } else {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, (alarms[i] * 1000))
+  }
 }
