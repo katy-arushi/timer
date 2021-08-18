442c81b1e4dc1c0b9c7beed132aeb4196fc1f5b1
Arushi Katyal <katy.arushi@gmail.com>
2021-08-17 23:34:51 -0400

    inital commit and finished

--- timer1.js
+++ timer1.js
@@ -3,5 +3,5 @@ const alarms = process.argv.slice(2);
 for (let i = 0; i < alarms.length; i++) {
   setTimeout(() => {
     process.stdout.write('\x07');
-  }, (alarms[i] * 1000));
+  }, (alarms[i] * 1000))
 }
